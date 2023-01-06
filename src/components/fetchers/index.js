a656ddefe078fbf5b374e9d616fda05f4981fd7b
NestorSanchez <nesaenz.69@gmail.com>
2023-01-06 11:02:27 -0500

    add new folder for use useContext and hook

--- src/components/fetchers/index.js
+++
@@ -1,5 +0,0 @@
-// import axios from "axios";
-
-// const getcities = async () => {
-//     let cities = await axios.get(`${process.env.NEXT_PUBLIC_URL}/cities`)
-// }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import Footer from '../components/Layout/Footer/Footer';
 import HomeTwoMain from '../components/HomeTwo/HomeTwoMain';
 import HeaderTwo from '../components/Layout/Header/HeaderStyleTwo';
-import { BASE_URL } from '../components/constans/dominio';
+
 
 const HomeTwo = () => {
-    console.log(BASE_URL)
     return (
         <React.Fragment>   
-            
             <HeaderTwo />
             <HomeTwoMain />
             <Footer />
