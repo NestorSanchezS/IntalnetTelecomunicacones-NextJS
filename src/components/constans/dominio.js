0c8e329ce39cbddfab98316847ac91e8bdbb93c5
NestorSanchez <nesaenz.69@gmail.com>
2023-01-05 21:11:24 -0500

    add environment variables .env.development and .env.production

--- src/components/constans/dominio.js
+++ src/components/constans/dominio.js
@@ -1,1 +1,3 @@
-export const BASE_URL = `${process.env.NEXT_PUBLIC_SITE_URL}`
+export const BASE_URL = `${process.env.NEXT_PUBLIC_SITE_URL}/api/v1`
+
+export const BASe_URL2= `${process.env.NEXT_PUBLIC_SITE_URL}`
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import Footer from '../components/Layout/Footer/Footer';
 import HomeTwoMain from '../components/HomeTwo/HomeTwoMain';
 import HeaderTwo from '../components/Layout/Header/HeaderStyleTwo';
+import { BASE_URL } from '../components/constans/dominio';
 
 const HomeTwo = () => {
+    console.log(BASE_URL)
     return (
         <React.Fragment>   
+            
             <HeaderTwo />
             <HomeTwoMain />
             <Footer />
