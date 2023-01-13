5b131fb21c306651d14b8d9e3a41b94335a713d9
NestorSanchez <nesaenz.69@gmail.com>
2023-01-12 20:37:41 -0500

    add new component in carpet HomeTwo, add PlanCitieService

--- src/components/HomeTwo/PricingSectionThree.js
+++ src/components/HomeTwo/PricingSectionThree.js
@@ -9,7 +9,7 @@ const PricingThree = () => {
                 <div className="row justify-content-center">
                     <div className="col-xl-5 col-lg-6 col-md-8">
                         <div className="section-title text-center mb-50">
-                            <h2 className="title">Discover Our Best Value Plans</h2>
+                            <h2 className="title">Descubra Nuestros Planes</h2>
                         </div>
                     </div>
                 </div>
