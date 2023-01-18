39502ec8421eb25e812a86a73889d74f07641fc0
NestorSanchez <nesaenz.69@gmail.com>
2023-01-18 15:58:59 -0500

    add new pages pqr, internetsano, useprotection

---
+++ src/pages/internetsano.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HeaderTwo from "../components/Layout/Header/HeaderStyleTwo";
+import Footer from "../components/Layout/Footer/Footer";
+
+const internetsano = () => {
+  return (
+    <React.Fragment>
+      <HeaderTwo />
+      <Footer />
+    </React.Fragment>
+  );
+};
+
+export default internetsano;
---
+++ src/pages/userprotection.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HeaderTwo from "../components/Layout/Header/HeaderStyleTwo";
+import Footer from "../components/Layout/Footer/Footer";
+
+const userprotection = () => {
+  return (
+    <React.Fragment>
+      <HeaderTwo />
+      <Footer />
+    </React.Fragment>
+  );
+};
+
+export default userprotection;
