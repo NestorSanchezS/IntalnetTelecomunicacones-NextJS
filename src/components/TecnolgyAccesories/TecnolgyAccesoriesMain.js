8311d3cc0555a2f7a49ca7948ce5e41903a29e71
NestorSanchez <nesaenz.69@gmail.com>
2023-01-18 18:48:09 -0500

    finiched componente BurgerMenus.js

---
+++ src/components/TecnolgyAccesories/TecnolgyAccesoriesMain.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Breadcrumb from "../Common/Breadcumb";
+
+export const TecnolgyAccesoriesMain = () => {
+  return (
+    <main>
+      <main>
+        <Breadcrumb pageTitle="Accesorios de Tecnologia" />
+      </main>
+    </main>
+  );
+};
