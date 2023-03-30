2ee3bd0e0e90cc5abb16abb50aef2fea89924729
NestorSanchez <nesaenz.69@gmail.com>
2023-03-30 08:45:32 -0500

    create component information

---
+++ src/components/Common/information.js
@@ -0,0 +1,74 @@
+import React from "react";
+import ModalVideo from "react-modal-video";
+
+export const information = () => {
+  return (
+    <section className="inner-services-area pt-120">
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-xl-7 col-lg-6 order-0 order-lg-2">
+            <div
+              className="services-inner-img wow fadeInRight"
+              data-wow-delay=".2s"
+            >
+              <img
+                src="/assets/img/images/pricing_item_bg01.jpg"
+                alt="img not found"
+              />
+              <ModalVideo
+                channel="youtube"
+                isOpen={isOpen}
+                videoId="R5ARkEAhI-k"
+                onClose={() => {
+                  openVideoModal();
+                }}
+              />
+              <div className="video-paly">
+                <a
+                  href="#!"
+                  className="popup-video"
+                  onClick={() => {
+                    openVideoModal();
+                  }}
+                >
+                  <i className="fas fa-play"></i>{" "}
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-5 col-lg-6">
+            <div className="section-title mb-30">
+              <h2 className="title">
+                Encuentra Las Mejores Soluciones de Red con Nosotros
+              </h2>
+            </div>
+            <div className="services-inner-content">
+              <p>
+                Internet residencial para conectar su computadora, celular y
+                otros dispositivos en su hogar simultáneamente
+              </p>
+              <ul className="services-inner-list">
+                <li>
+                  <img src="/assets/img/icons/check.png" alt="img not found" />{" "}
+                  Garantizamos un servicio de calidad.
+                </li>
+                <li>
+                  <img src="/assets/img/icons/check.png" alt="img not found" />{" "}
+                  Equipo Técnico Expecializado.
+                </li>
+                <li>
+                  <img src="/assets/img/icons/check.png" alt="img not found" />{" "}
+                  Tecnologia Avanzada
+                </li>
+                <li>
+                  <img src="/assets/img/icons/check.png" alt="img not found" />{" "}
+                  Internet Por Fibra Optica.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
