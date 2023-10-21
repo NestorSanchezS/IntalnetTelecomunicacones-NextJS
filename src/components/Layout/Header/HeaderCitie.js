148a3d9c652a28e407fd456a01c151dfc70f88e7
NestorSanchez <nesaenz.69@gmail.com>
2023-10-21 12:27:24 -0500

    exclude los cordoba in menu - hea

--- src/components/Layout/Header/HeaderCitie.js
+++ src/components/Layout/Header/HeaderCitie.js
@@ -3,7 +3,11 @@ import React from 'react'
 
 export const HeaderCitie = ({citie}) => {
     const {id, name} = citie;
-  return (
-    <li><Link href={`/services/${id}`}><a>{name}</a></Link></li>
-  )
+    if (name !== "Los Córdoba -Córdoba") {
+      return (
+        <li><Link href={`/services/${id}`}><a>{name}</a></Link></li>
+      )
+    } else {
+      return null;
+    }
 }
