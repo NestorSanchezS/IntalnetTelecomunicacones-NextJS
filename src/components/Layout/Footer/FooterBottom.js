7974550f7bc9ee9c3a110264a46f9542e2056f8a
Nestor Sanchez <nestor.sanchez@simetrik.com>
2025-10-08 21:04:23 -0500

    feat: change name

--- src/components/Layout/Footer/FooterBottom.js
+++ src/components/Layout/Footer/FooterBottom.js
@@ -13,7 +13,7 @@ const FooterBottom = () => {
                     </div>
                     <div className="col-md-8">
                         <div className="copyright-text text-center text-md-end">
-                            <p>Copyrighted 2023_INTALNET - Desarollado por Néstor Sánchez</p>
+                            <p>Copyrighted 2023_INTALNET - Desarrollado por Néstor Sánchez S.</p>
                         </div>
                     </div>
                 </div>
